19e5da6b491f8610c6c046266729ff621bdafb5b
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2025-01-27 16:20:19 +0530

    create a sales dashboard

--- custom_addons/odoo_custom_dashboard-master/static/src/components/sales_dashboard.js
+++ custom_addons/odoo_custom_dashboard-master/static/src/components/sales_dashboard.js
@@ -1,8 +1,8 @@
 /** @odoo-module */
 
 import { registry } from "@web/core/registry"
-import { KpiCard } from "./kpi_card/kpi_card"
-import { ChartRenderer } from "./chart_renderer/chart_renderer"
+// import { KpiCard } from "./kpi_card/kpi_card"
+// import { ChartRenderer } from "./chart_renderer/chart_renderer"
 import { loadJS } from "@web/core/assets"
 const { Component, onWillStart, useRef, onMounted } = owl
 
@@ -13,6 +13,6 @@ export class OwlSalesDashboard extends Component {
 }
 
 OwlSalesDashboard.template = "owl.OwlSalesDashboard"
-OwlSalesDashboard.components = { KpiCard, ChartRenderer }
+// OwlSalesDashboard.components = { KpiCard, ChartRenderer }
 
 registry.category("actions").add("owl.sales_dashboard", OwlSalesDashboard)
